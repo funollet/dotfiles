49a7eacaa075f8e0d4424c38fd480ca2398064ad
Jordi Funollet <funollet@fastmail.fm>
2018-09-19 10:44:43 +0200

    install task as binary, not package (conflicts with taskwarrior)

--- installers/bootstrap.sh
+++ installers/bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-version="2.1.0"
-url="https://github.com/go-task/task/releases/download/v${version}/task_linux_amd64.rpm"
+version="2.1.1"
+url="https://github.com/go-task/task/releases/download/v${version}/task_linux_amd64.tar.gz"
 
 which task > /dev/null && exit
 
 cd /tmp || exit
 wget ${url} 
-sudo dnf install -y task_linux_amd64.rpm
-
+tar -xzf task_linux_amd64.tar.gz
+sudo install task /usr/local/bin
