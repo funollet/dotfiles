14a60ed7ad36357905ec214b418e68c7a0dbdcaf
Jordi Funollet <funollet@fastmail.fm>
2019-02-08 16:37:38 +0100

    changes

--- bash/.bash.d/wallapop.bash
+++ bash/.bash.d/wallapop.bash
@@ -3,11 +3,18 @@
 # Config for wallapop-aws-credentials.
 test -f 00.bash.secrets && source 00.bash.secrets
 
-alias wallapop-aws-credentials="docker run --rm -it \
-    -v ~/.aws/credentials:/data/credentials \
-        -e PROFILE=production \
-        -e GOOGLE_USERNAME=\${WPOP_GOOGLE_USERNAME} \
-        -e GOOGLE_IDP_ID=\${WPOP_GOOGLE_IDP_ID} \
-        -e GOOGLE_SP_ID=\${WPOP_GOOGLE_SP_ID} \
-        -e REGION=eu-west-1 \
-    wallapop/federated-credentials:latest"
+# alias wallapop-aws-credentials="docker run --rm -it \
+#     -v ~/.aws/credentials:/data/credentials \
+#         -e PROFILE=production \
+#         -e GOOGLE_USERNAME=\${WPOP_GOOGLE_USERNAME} \
+#         -e GOOGLE_IDP_ID=\${WPOP_GOOGLE_IDP_ID} \
+#         -e GOOGLE_SP_ID=\${WPOP_GOOGLE_SP_ID} \
+#         -e REGION=eu-west-1 \
+#     wallapop/federated-credentials:latest"
+
+alias wallapop-aws-credentials="aws-google-auth \
+    --username \${WPOP_GOOGLE_USERNAME} \
+    --idp-id \${WPOP_GOOGLE_IDP_ID} \
+    --sp-id \${WPOP_GOOGLE_SP_ID} \
+    --region eu-west-1 \
+    --profile production"
