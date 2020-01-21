a19507049239d6b3f9a79733786768abddaedab3
Jordi Funollet <funollet@fastmail.fm>
2020-01-21 13:46:01 +0100

    i3 + kde, first iteration

--- kde/.config/plasma-workspace/env/set_window_manager.sh
+++ kde/.config/plasma-workspace/env/set_window_manager.sh
@@ -2,5 +2,5 @@
 
 export TERMINAL=konsole                 # just for i3
 
-# export KDEWM=/usr/bin/i3                # i3
+export KDEWM=/usr/bin/i3                # i3
 # export KDEWM=/usr/bin/awesome           # awesome
