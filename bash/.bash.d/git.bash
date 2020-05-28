a0bb5acc13c2c981b8940b257837feb8a69a5654
Jordi Funollet <funollet@fastmail.fm>
2020-05-28 15:02:03 +0200

    changes

---
+++ bash/.bash.d/git.bash
@@ -0,0 +1,13 @@
+
+
+## https://thoughtbot.com/blog/powerful-git-macros-for-automating-everyday-workflows
+#
+gbc() {
+  git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative $@..$(git rev-parse --abbrev-ref HEAD)
+}
+
+gbbc() {
+  git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative $(git rev-parse --abbrev-ref HEAD)..$@
+}
+#
+##
