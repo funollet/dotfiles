002f224763ff1b1d6050559428d651a569b28370
Jordi Funollet <funollet@fastmail.fm>
2020-08-12 17:28:34 +0200

    changes

--- bash/.bash.d/wallapop.bash
+++ bash/.bash.d/wallapop.bash
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-# Config for wallapop-aws-credentials.
+# Config for AWS credentials:
 # test -f ~/.bash.d/00.bash.secrets && source ~/.bash.d/00.bash.secrets
 # 
-# alias wallapop-aws-credentials="docker run --rm -it \
-#     -v ~/.aws/credentials:/root/credentials \
-#         -e GOOGLE_USERNAME=\${GOOGLE_USERNAME} \
-#         -e GOOGLE_IDP_ID=\${GOOGLE_IDP_ID} \
-#         -e GOOGLE_SP_ID=\${GOOGLE_SP_ID} \
-#         -e REGION=eu-west-1 \
-#     wallapop/federated-credentials:latest --log debug"
-
 # Running locally:
 # No need to load 00.bash.secrets: just provide the values on the first run and
 # those will be cached on ~/.aws/config.
 #
 #     aws-google-auth --region eu-west-1
-
-alias cfn-lint="cfn-lint -a ~/code/wallapop/platform/docker-base-images/cfn-lint/custom_rules/"
-
-ec2listenvs () {
-    aws ec2 describe-instances --output json --filters "Name=tag-key,Values=Env" \
-        | jq '.Reservations[].Instances[].Tags[] | select(.Key=="Env")' \
-        | grep Value | sort | uniq
-}
