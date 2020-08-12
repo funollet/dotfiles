68ffdf19ff29208f883376148faf135a6f76f1ca
Jordi Funollet <funollet@fastmail.fm>
2020-08-12 18:10:10 +0200

    changes

--- bash/.bash.d/git.fzf.bash
+++ bash/.bash.d/git.fzf.bash
@@ -2,6 +2,8 @@
 
 bind '"\er": redraw-current-line'
 bind '"\C-g\C-f": "$(gf)\e\C-e\er"'
+bind '"\C-g\C-b": "$(gb)\e\C-e\er"'
+bind '"\C-g\C-v": "$(fshow)\e\C-e\er"'
 
 gco() {
     # are we in a in git repo?
@@ -21,9 +23,9 @@ gb() {
     # are we in a git repo?
     git rev-parse HEAD > /dev/null 2>&1 || return
   
-    git branch -a --color=always | grep -v '/HEAD\s' | sort |
-    fzf --height 50% "$@" --border --ansi --multi --tac --preview-window right:70% \
-        --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
+    git branch --color=always | grep -v '/HEAD\s' | sort |
+    fzf --height 50% "$@" --border --ansi --multi --tac --preview-window right:70% | \
+        #--preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
     sed 's/^..//' | cut -d' ' -f1 |
     sed 's#^remotes/##'
 }
@@ -38,3 +40,12 @@ gf() {
     cut -c4- | sed 's/.* -> //'
 }
 
+
+# fshow - get commit id
+fshow() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index \
+      --preview 'git diff --color=always {2} | head -500' \
+    | grep -o '[a-f0-9]\{7\}' | head -1
+}
