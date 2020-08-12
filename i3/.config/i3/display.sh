e5b16dca48909314d6a7f064e68773bc2fbf935c
Jordi Funollet <funollet@fastmail.fm>
2020-08-12 17:41:06 +0200

    changes

--- i3/.config/i3/display.sh
+++ i3/.config/i3/display.sh
@@ -5,7 +5,7 @@ setup_displays () {
     if [ "$(hostname)" = "yak.lan" ] ; then
         if xrandr | grep -q 'HDMI-1 connected' ; then
             xrandr --output HDMI-1 --primary --auto \
-                   --output eDP-1 --auto --left-of HDMI-1 --mode 1920x1080
+                   --output eDP-1 --auto --right-of HDMI-1 --mode 1920x1080
         else
             xrandr --output HDMI-1 --off --output eDP-1 --primary --mode 1920x1080
         fi
