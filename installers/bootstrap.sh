4ea593407af67fbdf02a950b437b9bc3e89ac870
Jordi Funollet <funollet@fastmail.fm>
2020-12-16 17:06:16 +0100

    changes

--- installers/bootstrap.sh
+++ installers/bootstrap.sh
@@ -1,16 +1,3 @@
 #!/bin/bash -e
 
-# linuxbrew dependencies
-sudo yum groupinstall 'Development Tools'
-sudo yum install curl file git
-sudo yum install libxcrypt-compat # needed by Fedora 30 and up
-
-# Linuxbrew
-if ! command -v brew > /dev/null ; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
-
-# go-task
-if ! command -v task > /dev/null ; then
-  brew install go-task/tap/go-task
-fi
+sudo yum install ansible
