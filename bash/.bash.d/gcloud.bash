b839594f059141233a45db860df43cac562e47a6
Jordi Funollet <funollet@fastmail.fm>
2022-07-27 15:12:54 +0200

    changes

--- bash/.bash.d/gcloud.bash
+++ bash/.bash.d/gcloud.bash
@@ -6,3 +6,14 @@
 #     gcloud config configurations list | fzf +m --header-lines=1 +s | awk '{print $1}' | \
 #         xargs --no-run-if-empty gcloud config configurations activate
 # }
+
+alias gssh='gcloud compute ssh'
+alias gscp='gcloud compute scp'
+
+_complete_gcloud_instance () {
+    instance="$(ginstance)"
+    COMPREPLY+=("$instance")
+}
+
+complete -F _complete_gcloud_instance gssh
+complete -F _complete_gcloud_instance gscp
