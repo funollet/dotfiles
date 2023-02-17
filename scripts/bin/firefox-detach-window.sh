b58655297f739e47117612555b41d569d7a03859
Jordi Funollet <funollet@fastmail.fm>
2023-02-17 18:04:46 +0100

    i3 hotkey and script to move a firefox tab to another screen

---
+++ scripts/bin/firefox-detach-window.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# firefox-detach-window.sh
+#
+# Move a Firefox tab to its own window and send it to another screen.
+
+
+# go to url 
+# shift+tab backwards to the tab
+# use the contextual menu to detach the tab
+xdotool sleep 500 key --delay 500 \
+  ctrl+l \
+  shift+Tab shift+Tab shift+Tab shift+Tab \
+  shift+F10 v w
+
+# set focus on the newest Firefox window
+sleep 0.8
+firefox_last_window=$(wmctrl -l | awk '/ — Mozilla Firefox$/ {print $1}' | sort -n | tail -1)
+wmctrl -i -a "$firefox_last_window"
+
+sleep .2
+i3-msg "move container to output next"
+
+# set focus on the newest Firefox window
+sleep .2
+wmctrl -i -a "$firefox_last_window"
