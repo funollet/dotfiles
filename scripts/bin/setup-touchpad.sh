8db7043a09bdb743b7848a0fc1241d8282b3c70a
Jordi Funollet <funollet@fastmail.fm>
2024-02-09 11:37:51 +0100

    touchpad

--- scripts/bin/setup-touchpad.sh
+++ scripts/bin/setup-touchpad.sh
@@ -6,7 +6,7 @@ devs=$(xinput list | grep -Ei 'magic trackpad|touchpad' | sed 's/.*id=//' | awk 
 for dev in ${devs} ; do
   xinput set-prop "${dev}" "libinput Tapping Enabled" 1
   xinput set-prop "${dev}" "libinput Natural Scrolling Enabled" 0
-  xinput set-button-map "${dev}" 1 3 2 4 5 6 7
+  xinput set-button-map "${dev}" 1 2 3 4 5 6 7
 done
 
 systemctl --user restart fusuma
