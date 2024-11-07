1c4a8911120a6560d52fbaad9937bfebe369608d
Jordi Funollet <funollet@fastmail.fm>
2024-11-07 16:19:43 +0100

    qtile: formatting; also, mmedia keys manage youtube in browser, too

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -16,11 +16,12 @@ mod = "mod4"
 terminal = guess_terminal()
 
 colors = {
-    'white':    '#ffffff',
-    'red':      'ff0000',
-    'yellow':   'c18f41',
-    'grey':     '404040',
+    "white": "#ffffff",
+    "red": "ff0000",
+    "yellow": "c18f41",
+    "grey": "404040",
 }
+
 
 def window_to_next_screen(qtile, switch_group=False, switch_screen=False):
     i = qtile.screens.index(qtile.current_screen)
@@ -33,11 +34,13 @@ def window_to_next_screen(qtile, switch_group=False, switch_screen=False):
     if switch_screen:
         qtile.to_screen(dest_idx)
 
+
 @lazy.function
 def prev_group_or_stay(qtile):
     current_index = qtile.groups.index(qtile.current_group)
     if current_index > 0:
         qtile.current_screen.prev_group(skip_empty=True)
+
 
 @lazy.function
 def next_group_or_stay(qtile):
@@ -46,6 +49,7 @@ def next_group_or_stay(qtile):
     if current_index < last_index:
         qtile.current_screen.next_group(skip_empty=True)
 
+
 @lazy.function
 def float_to_front(qtile):
     lazy.window.toggle_floating()
@@ -53,17 +57,16 @@ def float_to_front(qtile):
         if window.floating:
             window.cmd_bring_to_front()
 
- 
+
 def move_window_to_group(direction):
     current_group, groups = qtile.current_group, qtile.groups
- 
+
     current_index = groups.index(current_group)
     target_index = (current_index + direction) % len(groups)
     target_group = groups[target_index].name
- 
+
     if qtile.current_window:
         qtile.current_window.togroup(target_group, switch_group=True)
-
 
 
 keys = [
@@ -74,8 +77,9 @@ keys = [
     Key("M-k", lazy.layout.up(), desc="Move focus up"),
     #
     Key("M-<space>", lazy.layout.next(), desc="Move window focus to other window"),
-    Key("M-S-<space>", lazy.layout.previous(), desc="Move window focus to other window"),
-
+    Key(
+        "M-S-<space>", lazy.layout.previous(), desc="Move window focus to other window"
+    ),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key("M-C-h", lazy.layout.shuffle_left(), desc="Move window to the left"),
@@ -83,7 +87,6 @@ keys = [
     Key("M-C-j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key("M-C-k", lazy.layout.shuffle_up(), desc="Move window up"),
     Key("M-C-<Return>", lazy.layout.swap_main(), desc="Swap window to main partition"),
-
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key("M-C-S-h", lazy.layout.shrink_main(), desc="Shrink main window"),
@@ -91,21 +94,37 @@ keys = [
     Key("M-C-S-j", lazy.layout.grow(), desc="Grow window"),
     Key("M-C-S-k", lazy.layout.shrink(), desc="Shrink window"),
     Key("M-C-S-n", lazy.layout.normalize(), desc="Reset all window sizes"),
-
     # Group management.
     Key("M-u", prev_group_or_stay, desc="Switch to previous group"),
     Key("M-i", next_group_or_stay, desc="Switch to next group"),
-    Key("M-C-u", lazy.function(lambda q: move_window_to_group(-1)), desc="Move window to previous group"),
-    Key("M-C-i", lazy.function(lambda q: move_window_to_group(1)), desc="Move window to next group"),
-
-    Key("M-o", lazy.next_screen(), desc='Next monitor'),
-    Key("M-C-o", lazy.function(window_to_next_screen, switch_screen=True), desc='Move window to next monitor'),
-
+    Key(
+        "M-C-u",
+        lazy.function(lambda q: move_window_to_group(-1)),
+        desc="Move window to previous group",
+    ),
+    Key(
+        "M-C-i",
+        lazy.function(lambda q: move_window_to_group(1)),
+        desc="Move window to next group",
+    ),
+    Key("M-o", lazy.next_screen(), desc="Next monitor"),
+    Key(
+        "M-C-o",
+        lazy.function(window_to_next_screen, switch_screen=True),
+        desc="Move window to next monitor",
+    ),
     # Layout management.
     Key("M-<Tab>", lazy.next_layout(), desc="Toggle between layouts"),
-    Key("M-f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen on the focused window"),
-    Key("M-t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
-
+    Key(
+        "M-f",
+        lazy.window.toggle_fullscreen(),
+        desc="Toggle fullscreen on the focused window",
+    ),
+    Key(
+        "M-t",
+        lazy.window.toggle_floating(),
+        desc="Toggle floating on the focused window",
+    ),
     # Qtile management.
     Key("M-q", lazy.window.kill(), desc="Kill focused window"),
     Key("M-A-r", lazy.reload_config(), desc="Reload the config"),
@@ -117,26 +136,47 @@ groups = [Group(i) for i in "123456789"]
 
 for i in groups:
     keys += [
-        Key(f"M-{i.name}", lazy.screen.toggle_group(i.name),
-            desc=f"Switch to group {i.name}"),
-        Key(f"M-C-{i.name}", lazy.window.togroup(i.name, switch_group=True),
-            desc=f"Switch to & move focused window to group {i.name}"),
+        Key(
+            f"M-{i.name}",
+            lazy.screen.toggle_group(i.name),
+            desc=f"Switch to group {i.name}",
+        ),
+        Key(
+            f"M-C-{i.name}",
+            lazy.window.togroup(i.name, switch_group=True),
+            desc=f"Switch to & move focused window to group {i.name}",
+        ),
     ]
 
-groups.append(ScratchPad('scratchpad', [
-    # DropDown('slack', 'flatpak run com.slack.Slack',
-    #          match=Match(wm_class=['Slack']),
-    #          width=0.8, height=0.95, x=0.1, y=0.025,
-    #          on_focus_lost_hide=False),
-    DropDown('telegram', 'flatpak run org.telegram.desktop',
-             match=Match(wm_class=["telegram-desktop", "TelegramDesktop"]),
-             width=0.8, height=1.0,
-             on_focus_lost_hide=False),
-    DropDown('spotify', 'flatpak run com.spotify.Client',
-             match=Match(wm_class=["spotify", "Spotify"]),
-             width=1.0, height=1.0, x=0, y=0,
-             on_focus_lost_hide=False),
-]))
+groups.append(
+    ScratchPad(
+        "scratchpad",
+        [
+            # DropDown('slack', 'flatpak run com.slack.Slack',
+            #          match=Match(wm_class=['Slack']),
+            #          width=0.8, height=0.95, x=0.1, y=0.025,
+            #          on_focus_lost_hide=False),
+            DropDown(
+                "telegram",
+                "flatpak run org.telegram.desktop",
+                match=Match(wm_class=["telegram-desktop", "TelegramDesktop"]),
+                width=0.8,
+                height=1.0,
+                on_focus_lost_hide=False,
+            ),
+            DropDown(
+                "spotify",
+                "flatpak run com.spotify.Client",
+                match=Match(wm_class=["spotify", "Spotify"]),
+                width=1.0,
+                height=1.0,
+                x=0,
+                y=0,
+                on_focus_lost_hide=False,
+            ),
+        ],
+    )
+)
 
 layout_defaults = {
     "border_focus": colors["yellow"],
@@ -147,6 +187,14 @@ layouts = [
     layout.Max(**layout_defaults),
     layout.MonadWide(**layout_defaults),
 ]
+
+Group(
+    "2",
+    layouts=[
+        layout.Max(**layout_defaults),
+        layout.MonadWide(**layout_defaults),
+    ],
+)
 
 widget_defaults = dict(
     font="sans",
@@ -172,14 +220,14 @@ screens = [
                 widget.Systray(),
                 widget.Spacer(),
                 widget.GroupBox(
-                    highlight_method='line',
+                    highlight_method="line",
                     this_current_screen_border=colors["yellow"],
                 ),
                 widget.Spacer(),
                 widget.Clock(
                     format="%I:%M",
                     fontsize=16,
-                    foreground=colors['white'],
+                    foreground=colors["white"],
                 ),
             ],
             24,
@@ -198,7 +246,7 @@ screens = [
                 ),
                 widget.Spacer(),
                 widget.GroupBox(
-                    highlight_method='line',
+                    highlight_method="line",
                     this_current_screen_border=colors["yellow"],
                 ),
                 widget.Spacer(),
@@ -211,7 +259,11 @@ screens = [
 
 mouse = [
     # Drag floating layouts.
-    Drag("M-Mouse-1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag(
+        "M-Mouse-1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
     Drag("M-Mouse-3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
     # Bring window to front.
     Click("M-Mouse-2", lazy.window.bring_to_front()),
@@ -247,10 +299,10 @@ reconfigure_screens = True
 # If things like steam games want to auto-minimize themselves when losing
 # focus, should we respect this or not?
 auto_minimize = True
- 
+
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
- 
+
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
 # mailing lists, GitHub issues, and other WM documentation that suggest setting
@@ -260,56 +312,110 @@ wl_input_rules = None
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
- 
- 
- 
+
+
 @hook.subscribe.startup_once
 def autostart():
-    script = Path('~/.config/qtile/autostart.sh').expanduser()
+    script = Path("~/.config/qtile/autostart.sh").expanduser()
     subprocess.Popen([script])
 
 
+@hook.subscribe.client_new
+def move_to_top_modals(window):
+    state = window.window.get_net_wm_state()
+    if state and "_NET_WM_STATE_MODAL" in state:
+        window.move_to_top()
+        window.keep_above()
+        window.focus()
+        # window.bring_to_front()
+        # raise Exception(window.info())
 
 
 keys += [
-    Key("<f7>", lazy.group['scratchpad'].dropdown_toggle('telegram'), desc="Toggle telegram"),
-    Key("<f8>", lazy.group['scratchpad'].dropdown_toggle('slack'), desc="Toggle slack"),
-    Key("<f9>", lazy.group['scratchpad'].dropdown_toggle('spotify'), desc="Toggle spotify"),
-
+    Key(
+        "<f7>",
+        lazy.group["scratchpad"].dropdown_toggle("telegram"),
+        desc="Toggle telegram",
+    ),
+    Key("<f8>", lazy.group["scratchpad"].dropdown_toggle("slack"), desc="Toggle slack"),
+    Key(
+        "<f9>",
+        lazy.group["scratchpad"].dropdown_toggle("spotify"),
+        desc="Toggle spotify",
+    ),
     Key("<f1>", lazy.spawn("mute-meet.sh"), desc="Mute/unmute video call"),
-    Key("<XF86Launch8>", lazy.spawn("firefox-detach-window.sh"), desc="Detach tab on firefox"),
+    Key(
+        "<XF86Launch8>",
+        lazy.spawn("firefox-detach-window.sh"),
+        desc="Detach tab on firefox",
+    ),
     Key("<XF86Launch7>", lazy.spawn("copypaster copy"), desc="Custom copy"),
     Key("<XF86Launch6>", lazy.spawn("copypaster paste"), desc="Custom paste"),
-
-    Key("<XF86AudioRaiseVolume>", lazy.spawn("volume-notify --step 3 up"), desc="Raise volume"),
-    Key("<XF86AudioLowerVolume>", lazy.spawn("volume-notify --step 3 down"), desc="Lower volume"),
+    Key(
+        "<XF86AudioRaiseVolume>",
+        lazy.spawn("volume-notify --step 3 up"),
+        desc="Raise volume",
+    ),
+    Key(
+        "<XF86AudioLowerVolume>",
+        lazy.spawn("volume-notify --step 3 down"),
+        desc="Lower volume",
+    ),
     Key("<XF86AudioMute>", lazy.spawn("volume-notify mute"), desc="Mute volume"),
     Key("M-<Prior>", lazy.spawn("volume-notify --step 3 up"), desc="Raise volume"),
     Key("M-<Next>", lazy.spawn("volume-notify --step 3 down"), desc="Lower volume"),
     Key("M-m", lazy.spawn("volume-notify mute"), desc="Mute volume"),
-
-    Key("<XF86AudioPlay>", lazy.spawn('playerctl -p spotify play-pause'), lazy.spawn('notify-send -t 3000 "Spotify: toggle-play"')),
-    Key("<XF86AudioNext>", lazy.spawn('playerctl -p spotify next')),
-    Key("<XF86AudioPrev>", lazy.spawn('playerctl -p spotify previous')),
-    KeyChord("M-p",
+    Key(
+        "<XF86AudioPlay>",
+        lazy.spawn(
+            "playerctl -l | grep firefox && playerctl -l | grep firefox | xargs playerctl play-pause -p || playerctl -p spotify play-pause",
+            shell=True,
+        ),
+        # lazy.spawn("playerctl -p spotify play-pause"),
+        lazy.spawn('notify-send -t 3000 "Toggle play/pause"'),
+        desc="Toggle play/pause on Youtube, if it's open; otherwise play/pause on Spotify",
+    ),
+    Key("<XF86AudioNext>", lazy.spawn("playerctl -p spotify next")),
+    Key("<XF86AudioPrev>", lazy.spawn("playerctl -p spotify previous")),
+    KeyChord(
+        "M-p",
         [
-            Key("k", lazy.spawn('playerctl -p spotify play-pause'), lazy.spawn('notify-send -t 3000 "Spotify: toggle-play"')),
-            Key("<space>", lazy.spawn('playerctl -p spotify play-pause'), lazy.spawn('notify-send -t 3000 "Spotify: toggle-play"')),
-            Key("j", lazy.spawn('playerctl -p spotify previous')),
-            Key("l", lazy.spawn('playerctl -p spotify next')),
-            Key("<Prior>", lazy.spawn('playerctl -p spotify previous')),
-            Key("<Next>", lazy.spawn('playerctl -p spotify next')),
+            Key(
+                "k",
+                lazy.spawn("playerctl -p spotify play-pause"),
+                lazy.spawn('notify-send -t 3000 "Spotify: toggle-play"'),
+            ),
+            Key(
+                "<space>",
+                lazy.spawn("playerctl -p spotify play-pause"),
+                lazy.spawn('notify-send -t 3000 "Spotify: toggle-play"'),
+            ),
+            Key("j", lazy.spawn("playerctl -p spotify previous")),
+            Key("l", lazy.spawn("playerctl -p spotify next")),
+            Key("<Prior>", lazy.spawn("playerctl -p spotify previous")),
+            Key("<Next>", lazy.spawn("playerctl -p spotify next")),
         ],
     ),
     Key("M-<Return>", lazy.spawn(terminal), desc="Launch terminal"),
     Key("M-S-a", lazy.spawn("autorandr 1"), desc="Call autorandr"),
     Key("M-r", lazy.spawn("ulauncher-toggle"), desc="Call ulauncher"),
-    Key("M-<Escape>", lazy.spawn("ulauncher-toggle"),
+    Key(
+        "M-<Escape>",
+        lazy.spawn("ulauncher-toggle"),
         lazy.spawn("xdotool sleep 0.2 type --clearmodifiers 'sm '"),
-        desc="Call ulauncher plugin for logout, reboot, etc."),
+        desc="Call ulauncher plugin for logout, reboot, etc.",
+    ),
     Key("C-A-l", lazy.spawn("xset s activate"), desc="Activate screensaver"),
     Key("M-y", lazy.spawn("dolphin"), desc="Call dolphin"),
     Key("<Print>", lazy.spawn("flameshot gui"), desc="Take a screenshot"),
-    Key("<XF86MonBrightnessDown>", lazy.spawn("light -U 15"), desc="Decrease screen brightness"),
-    Key("<XF86MonBrightnessUp>", lazy.spawn("light -A 15"), desc="Increase screen brightness"),
+    Key(
+        "<XF86MonBrightnessDown>",
+        lazy.spawn("light -U 15"),
+        desc="Decrease screen brightness",
+    ),
+    Key(
+        "<XF86MonBrightnessUp>",
+        lazy.spawn("light -A 15"),
+        desc="Increase screen brightness",
+    ),
 ]
