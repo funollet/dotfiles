0e440af16533b844054122699a89d0d546bfec60
Jordi Funollet <funollet@fastmail.fm>
2025-02-14 16:11:09 +0100

    cleanup brew-related files

--- bash/.bash.d/brew.bash
+++
@@ -1,8 +0,0 @@
-export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew";
-export HOMEBREW_CELLAR="/home/linuxbrew/.linuxbrew/Cellar";
-export HOMEBREW_REPOSITORY="/home/linuxbrew/.linuxbrew/Homebrew";
-export MANPATH="/home/linuxbrew/.linuxbrew/share/man${MANPATH+:$MANPATH}:";
-export INFOPATH="/home/linuxbrew/.linuxbrew/share/info${INFOPATH+:$INFOPATH}";
-export PATH="${PATH}:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin";
-
-for rc in /home/linuxbrew/.linuxbrew/etc/bash_completion.d/* ; do source ${rc} ; done
