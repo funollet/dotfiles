b8f62e66ad1b08a44cf409249fc3ee8ace6d546b
Jordi Funollet <funollet@fastmail.fm>
2025-07-15 11:38:16 +0200

    mute-meet.sh

--- scripts/bin/mute-meet.sh
+++ scripts/bin/mute-meet.sh
@@ -9,31 +9,31 @@ lockdir="/tmp/$(basename $0).lock"
 mkdir "${lockdir}" || exit 1
 trap "rmdir $lockdir" EXIT
 
+
 get_active_call () {
-  wmctrl -l | \
-    grep -E ' Slack call with| Zoom Meeting$| Meet – | - Discord$' | \
-    grep -Eio '(slack|zoom|meet)' | uniq | \
+  xdotool search --name \
+      'Slack call with|Zoom Meeting$|^Meet - | - Discord$' getwindowname %@ | \
+    grep -Eio '(slack|zoom|meet|discord)' | uniq | \
     tr '[:upper:]' '[:lower:]'
 }
 
 focus_and_toggle_audio () {
-    # get focus on the meeting window
-    wmctrl -a "$1"
-    sleep .1
-    # send hotkeys to toggle the mic
-    xdotool key "$2"
-    # get focus back to the original window
-    xdotool windowactivate "$current"
+    local current
+    current=$(xdotool getwindowfocus)
+
+    xdotool \
+      search --name "$1" \
+      windowfocus key "$2" \
+      windowfocus ${current}
 }
 
-current=$(xdotool getwindowfocus)
 
 active_call=$(get_active_call)
 
 case ${active_call} in
   slack)    focus_and_toggle_audio 'Slack call with' 'm' ;;
   zoom)     focus_and_toggle_audio 'Zoom Meeting' 'alt+a' ;;
-  meet)     focus_and_toggle_audio 'Meet' 'ctrl+d' ;;
+  meet)     focus_and_toggle_audio 'Meet -' 'ctrl+d' ;;
   discord)  focus_and_toggle_audio 'Discord' 'ctrl+shift+h' ;;
 esac
 
