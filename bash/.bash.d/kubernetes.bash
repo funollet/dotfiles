40ec1dc9d2f99c52df768775dad30c4594293768
Jordi Funollet <funollet@fastmail.fm>
2025-10-23 14:19:15 +0200

    kubectl completion without fzf

--- bash/.bash.d/kubernetes.bash
+++ bash/.bash.d/kubernetes.bash
@@ -2,13 +2,9 @@
 
 export KUBECONFIG=~/.kube/config
 
-which kubectl > /dev/null 2>&1 || return
+which kubectl >/dev/null 2>&1 || return
 
-if command -v fzf >/dev/null 2>&1 ; then
-	source <(kubectl completion bash | sed 's#"${requestComp}" 2>/dev/null#"${requestComp}" 2>/dev/null | head -n -1 | fzf  --multi=0 #g')
-else
-  source <(kubectl completion bash)
-fi
+source <(kubectl completion bash)
 
 # Replaced this alias by a symlink on ~/bin
 # alias k=kubectl
@@ -18,16 +14,15 @@ complete -F __start_kubectl k
 # Run k8set on Alt-k.
 bind -x '"\ek":"_kube_fzf_resource_selector"'
 
-
-k8labels () {
-    kubectl get pods --show-labels | grep "$@"
+k8labels() {
+  kubectl get pods --show-labels | grep "$@"
 }
 
-k8startedAt () {
-    # Show state.running.startedAt.
-    local pods
-    pods=$(kubectl get pods | fzf --header-lines=1 -m | awk -vORS=' ' '{print "pod/" $1}')
-    kubectl get ${pods} --no-headers \
-        -o custom-columns=pod:.metadata.name,startedAt:'.status.containerStatuses[0].state.running.startedAt' \
-        --sort-by '.status.containerStatuses[0].state.running.startedAt'
+k8startedAt() {
+  # Show state.running.startedAt.
+  local pods
+  pods=$(kubectl get pods | fzf --header-lines=1 -m | awk -vORS=' ' '{print "pod/" $1}')
+  kubectl get ${pods} --no-headers \
+    -o custom-columns=pod:.metadata.name,startedAt:'.status.containerStatuses[0].state.running.startedAt' \
+    --sort-by '.status.containerStatuses[0].state.running.startedAt'
 }
