05e5e6080bd2362ebd47d34e1b0aa0fa023f68a5
Jordi Funollet <funollet@fastmail.fm>
2025-11-28 22:41:40 +0100

    remove pyenv

--- installers/roles/pyenv/files/bash.d/pyenv.bash
+++
@@ -1,9 +0,0 @@
-# Load pyenv automatically by adding
-# the following to ~/.bashrc:
-
-export PATH="$HOME/.pyenv/bin:$PATH"
-
-if command -v pyenv 1>/dev/null 2>&1 ; then
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-fi
