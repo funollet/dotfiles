e003d0873bab785569419bb719b6fd00073eea2c
Jordi Funollet <funollet@fastmail.fm>
2025-12-14 22:20:30 +0100

    qtile starts ulauncher.service

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -6,4 +6,5 @@ set -x
 # lock screen after [TIMEOUT] seconds, dim during [CYCLE] seconds
 xset s 1800 15 \
   && xss-lock -n /usr/libexec/xsecurelock/dimmer -l -- xsecurelock &
+systemctl --user start ulauncher.service &
 dex-autostart --autostart --environment qtile --search-paths ~/.config/autostart/ &
