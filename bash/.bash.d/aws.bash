8bb0eb4eb7c713f36238ba823bed2e66a5d28a1c
Jordi Funollet <funollet@fastmail.fm>
2026-01-21 12:29:46 +0100

    better management of AWS_PROFILE

--- bash/.bash.d/aws.bash
+++ bash/.bash.d/aws.bash
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-export AWS_PROFILE="default"
+# export AWS_PROFILE="default"
+
+awsprofile() {
+  # Interactive AWS profile picker.
+  profile=$(aws configure list-profiles | fzf --no-multi --prompt "Choose active AWS profile: " --header "[Current: $AWS_PROFILE]")
+  [ -n "$profile" ] && export AWS_PROFILE="$profile"
+}
 
 ## ec2-search-id () {
 ##     # Parallel search of an EC2 InstanceID on multiple accounts.
 ##     AWS_ACCOUNTS='parsing web rtb info sistemas'
-## 
+##
 ##     echo -n $AWS_ACCOUNTS \
 ##         | xargs -P0 -d' ' -n1 -i -- \
 ##             aws --profile {} ec2 describe-instances \
@@ -14,46 +20,46 @@ export AWS_PROFILE="default"
 ##             --output text
 ## }
 
-aaws-ec2-whois () {
+aaws-ec2-whois() {
   # $1: Private IP of the instance or instance_id
   id=$1
 
-  if [ "${id::2}" = 'i-' ] ; then
+  if [ "${id::2}" = 'i-' ]; then
     filter_name="instance-id"
   else
     filter_name="network-interface.addresses.private-ip-address"
   fi
   aws ec2 describe-instances --output json \
-      --filters "Name=${filter_name},Values=${id}" \
-  | jq --sort-keys '.Reservations[].Instances[] | (.Tags | from_entries) + {"InstanceId": .InstanceId, "PrivateIpAddress": .PrivateIpAddress, "ImageId": .ImageId, "AZ": .Placement.AvailabilityZone} '
+    --filters "Name=${filter_name},Values=${id}" \
+    | jq --sort-keys '.Reservations[].Instances[] | (.Tags | from_entries) + {"InstanceId": .InstanceId, "PrivateIpAddress": .PrivateIpAddress, "ImageId": .ImageId, "AZ": .Placement.AvailabilityZone} '
 }
 
-aaws-ec2-bytag () {
-    aws ec2 describe-instances --output json --filters "Name=tag:$1,Values=$2"
+aaws-ec2-bytag() {
+  aws ec2 describe-instances --output json --filters "Name=tag:$1,Values=$2"
 }
 
-aaws-prodbytag () {
-    aws ec2 describe-instances --output json --filters "Name=tag:$1,Values=$2" \
-        "Name=instance-state-name,Values=running,pending" "Name=tag:Env,Values=prod" \
-        --query "Reservations[].Instances[]"
+aaws-prodbytag() {
+  aws ec2 describe-instances --output json --filters "Name=tag:$1,Values=$2" \
+    "Name=instance-state-name,Values=running,pending" "Name=tag:Env,Values=prod" \
+    --query "Reservations[].Instances[]"
 }
 
-aaws-betabytag () {
-    aws ec2 describe-instances --output json --filters "Name=tag:$1,Values=$2" \
-        "Name=instance-state-name,Values=running,pending" "Name=tag:Env,Values=beta"  \
-        --query "Reservations[].Instances[]"
+aaws-betabytag() {
+  aws ec2 describe-instances --output json --filters "Name=tag:$1,Values=$2" \
+    "Name=instance-state-name,Values=running,pending" "Name=tag:Env,Values=beta" \
+    --query "Reservations[].Instances[]"
 }
 
-aaws-asgpick () {
-    ASGTARGET=$(aws autoscaling describe-auto-scaling-groups --output json | \
-        jq .AutoScalingGroups[].AutoScalingGroupName -r \
-        | fzf)
-    export ASGTARGET
-    echo $ASGTARGET
+aaws-asgpick() {
+  ASGTARGET=$(aws autoscaling describe-auto-scaling-groups --output json \
+    | jq .AutoScalingGroups[].AutoScalingGroupName -r \
+    | fzf)
+  export ASGTARGET
+  echo $ASGTARGET
 }
 
-aaws-ecr-login () {
-    aws ecr get-login-password | docker login \
-        --username AWS \
-        --password-stdin "745640521341.dkr.ecr.eu-west-1.amazonaws.com"
+aaws-ecr-login() {
+  aws ecr get-login-password | docker login \
+    --username AWS \
+    --password-stdin "745640521341.dkr.ecr.eu-west-1.amazonaws.com"
 }
