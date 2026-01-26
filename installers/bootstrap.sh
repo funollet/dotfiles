af72a6c4c7bb32a5acfc656bbbbedf198407d6ee
Jordi Funollet <funollet@fastmail.fm>
2026-01-26 08:55:32 +0100

    boostrap.sh: cleanup

--- installers/bootstrap.sh
+++ installers/bootstrap.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # bootstrap.sh
 
-
 set -eu
 
 sudo dnf install -y git curl just vim dnf-plugins-core vim-enhanced stow ansible
@@ -13,23 +12,11 @@ sudo dnf install -y mise
 eval "$(/usr/bin/mise activate bash)"
 
 # make global versions available
-cd .. ; stow mise ; cd $OLDPWD
+cd ..
+stow mise
+mise install
+cd $OLDPWD
 
 echo
 echo "#################"
 echo You may want to enable running some commands passwordless \(visudo\).
-
-# asdf plugin add pipx https://github.com/amrox/asdf-pyapp.git
-# asdf install pipx
-#
-# pipx install --include-deps ansible==5.2.0
-# ansible-galaxy collection install community.general
-# Should I inject boto?
-
-# Alternative, not yet working:
-
-#   asdf plugin add ansible https://github.com/amrox/asdf-pyapp.git
-#   asdf install ansible latest:5.
-#
-# - bins are installed on the venv but not exposed as shims
-# - how do I inject required libraries? (boto,...)
