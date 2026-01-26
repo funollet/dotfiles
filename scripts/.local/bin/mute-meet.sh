de565b6a381d0d6a0e9640c3aedb5833163d278a
Jordi Funollet <funollet@fastmail.fm>
2026-01-26 08:56:32 +0100

    mute-meet: cosmetic changes

--- scripts/.local/bin/mute-meet.sh
+++ scripts/.local/bin/mute-meet.sh
@@ -11,9 +11,9 @@ trap "rmdir $lockdir" EXIT
 
 get_active_call() {
   xdotool search --name \
-    'Slack call with|Zoom Meeting$|^Meet - | - Discord$' getwindowname %@ |
-    grep -Eio '(slack|zoom|meet|discord)' | uniq |
-    tr '[:upper:]' '[:lower:]'
+    'Slack call with|Zoom Meeting$|^Meet - | - Discord$' getwindowname %@ \
+    | grep -Eio '(slack|zoom|meet|discord)' | uniq \
+    | tr '[:upper:]' '[:lower:]'
 }
 
 focus_and_toggle_audio() {
@@ -34,10 +34,10 @@ focus_and_toggle_audio() {
 active_call=$(get_active_call)
 
 case ${active_call} in
-slack) focus_and_toggle_audio 'Slack call with' 'm' "${active_call}" ;;
-zoom) focus_and_toggle_audio 'Zoom Meeting' 'alt+a' "${active_call}" ;;
-meet) focus_and_toggle_audio 'Meet -' 'ctrl+d' "${active_call}" ;;
-discord) focus_and_toggle_audio 'Discord' 'ctrl+shift+h' "${active_call}" ;;
+  slack) focus_and_toggle_audio 'Slack call with' 'm' "${active_call}" ;;
+  zoom) focus_and_toggle_audio 'Zoom Meeting' 'alt+a' "${active_call}" ;;
+  meet) focus_and_toggle_audio 'Meet -' 'ctrl+d' "${active_call}" ;;
+  discord) focus_and_toggle_audio 'Discord' 'ctrl+shift+h' "${active_call}" ;;
 esac
 
 sleep .5
