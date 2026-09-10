1f01b9a1ea3e71d710ef86caf803ce8588773ffc
Jordi Funollet <funollet@fastmail.fm>
2026-09-10 18:48:13 +0200

    feat(yazi): add yazi file manager with cd-on-exit wrapper
    - add y() bash function to cd into yazi's last directory on exit
    - install yazi via mise
    - add empty yazi keymap and config placeholders

---
+++ bash/.bash.d/yazi.bash
@@ -0,0 +1,8 @@
+function y() {
+  local tmp cwd
+  tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+  command yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd <"$tmp"
+  [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd" || builtin true
+  command rm -f -- "$tmp"
+}
